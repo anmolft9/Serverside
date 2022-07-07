7bd7da73c1f677c5d7741dd226259ba45e8b9801
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2022-07-07 15:43:34 +1000

    added the fetching feature from the mongodb

--- src/routers/model/task/TaskModel.js
+++ src/routers/model/task/TaskModel.js
@@ -5,6 +5,12 @@ export const insertTask = (taskObj) => {
   return TaskSchema(taskObj).save();
 };
 //select query
+export const getTask = () => {
+  return TaskSchema.find();
+};
+export const getSingleTask = (_id) => {
+  return TaskSchema.findById(_id);
+};
 
 //update query
 
--- src/routers/taskRouter.js
+++ src/routers/taskRouter.js
@@ -1,30 +1,26 @@
 import express from "express";
-import { insertTask } from "./model/task/TaskModel.js";
+import { getSingleTask, getTask, insertTask } from "./model/task/TaskModel.js";
 
 const router = express.Router();
 
-let fakeDB = [
-  { _id: 1, task: "watching tv", hr: 30 },
-  { _id: 2, task: "watching mobile", hr: 3 },
-  { _id: 3, task: "watching tablet", hr: 2 },
-  { _id: 4, task: "watching football", hr: 2 },
-  { _id: 5, task: "watching a girl", hr: 2 },
-];
-
-router.get("/:_id?", (req, res, next) => {
+router.get("/:_id?", async (req, res, next) => {
   try {
+    // const { _id } = req.params;
+
+    // let data = fakeDB;
+
+    // if (_id) {
+    //   data = fakeDB.filter((item) => item._id === +_id);
+    // }
     const { _id } = req.params;
 
-    let data = fakeDB;
+    const result = _id ? await getSingleTask(_id) : await getTask();
 
-    if (_id) {
-      data = fakeDB.filter((item) => item._id === +_id);
-    }
     ///query the db and get all the task
     res.json({
       status: "success",
       message: "you hit the get root route",
-      data,
+      result,
     });
   } catch (error) {
     error.status = 500;
@@ -67,13 +63,13 @@ router.patch("/", (req, res, next) => {
 /////DELETE
 router.delete("/", (req, res, next) => {
   try {
-    const { _id } = req.body;
+    // const { _id } = req.body;
 
-    let filteredArg = fakeDB.filter((item) => item?._id !== +_id);
-    fakeDB = filteredArg;
-    // if (_id) {
-    //   data = fakeDB.filter((item) => item._id === +_id);
-    // }
+    // let filteredArg = fakeDB.filter((item) => item?._id !== +_id);
+    // fakeDB = filteredArg;
+    // // if (_id) {
+    // //   data = fakeDB.filter((item) => item._id === +_id);
+    // // }
 
     res.json({
       status: "success",
